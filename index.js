a04d87e5990df0d20ee57fcdfd6793be0ec76c55
Andrey K <karway211@gmail.com>
2019-12-17 02:04:39 +0300

    feat: add brightness change

--- index.js
+++ index.js
@@ -1,21 +1,26 @@
 const body = document.getElementsByTagName('body')[0];
 const root = document.createElement('div');
 root.id = 'root';
+const audio = document.createElement('audio');
+// audio.src = './assets/audio/Detsl.mp3';
+audio.src = './assets/audio/Metallica.mp3';
+body.appendChild(audio);
 body.appendChild(root);
 
-for (let i = 0; i < 25; i++) {
+
+for (let i = 0; i < 50; i++) {
     let square = document.createElement('div');
     square.classList.add('square');
     root.appendChild(square);
 }
 
 const allSquare = document.querySelectorAll('.square');
-console.log(allSquare);
+// console.log(allSquare);
 
 allSquare.forEach((el) => {
-    const r = Math.floor(Math.random() * (256)),
-        g = Math.floor(Math.random() * (256)),
-        b = Math.floor(Math.random() * (256)),
+    const r = Math.floor(Math.random() * (255)),
+        g = Math.floor(Math.random() * (255)),
+        b = Math.floor(Math.random() * (255)),
         color = `#${r.toString(16)}${g.toString(16)}${b.toString(16)}`;
         el.style.background = color;
 });
@@ -26,53 +31,43 @@ button.type = 'button';
 button.value = 'play';
 body.appendChild(button);
 
+let context, analyser, src, arr;
 
-const context = new window.AudioContext(); //
-let buffer, source, destination; 
-
-const loadSoundFile = function(url) {
-  const xhr = new XMLHttpRequest();
-  xhr.open('GET', url, true);
-  xhr.responseType = 'arraybuffer';
-  xhr.onload = function(e) {
-    context.decodeAudioData(this.response,
-    function(decodedArrayBuffer) {
-      buffer = decodedArrayBuffer;
-    }, function(e) {
-      console.log('Error decoding file', e);
-    });
-  };
-  xhr.send();
+function preparation(){
+  context = new AudioContext();
+  analyser = context.createAnalyser();
+  src = context.createMediaElementSource(audio);
+  src.connect(analyser);
+  analyser.connect(context.destination);
+  loop();
+}
+function loop(){
+  if(!audio.paused){
+    window.requestAnimationFrame(loop);
+  }
+  arr = new Uint8Array(50);
+  analyser.getByteFrequencyData(arr);
+  console.log(arr);
+  for (let i = 0; i < 50; i++){
+    const fg = arr[i] / Math.max.apply(null, arr);
+    // console.log(arr[i]);
+    // allSquare[i].style.opacity = `${fg}`
+    allSquare[i].style.filter = `brightness(${fg})`;
+  }
 }
 
-const play = function(){
-  source = context.createBufferSource();
-  source.buffer = buffer;
-  destination = context.destination;
-  source.connect(destination);
-  source.start(0);
+button.onclick = (e) => {
+  // console.log(e);
+  if(!context) {
+    preparation();
+  }
+  if(audio.paused){
+    audio.play();
+    button.value = 'pause';
+    loop();
+  } else {
+    audio.pause();
+    button.value = 'play';
+  }
 }
-
-const stop = function(){
-  source.stop(0);
-}
-
-loadSoundFile('./assets/audio/Detsl.mp3');
-
-// const analyser = context.createAnalyser();
-// analyser.fftSize = 2048;
-// bTimeData = new Uint8Array(analyser.frequencyBinCount);
-// analyser.getByteTimeDomainData(bTimeData);
-
-button.addEventListener('mouseup', (e) => {
-    if (e.target.value === 'play') {
-        console.log(e.target.value);
-        play();
-        button.value = 'stop';
-    } else if (e.target.value === 'stop') {
-        console.log(e.target.value);
-        stop();
-        button.value = 'play';
-    }
-});
 
