eb34eaf771c9d7974b661f687bd941217f29d8ba
Andrey K <karway211@gmail.com>
2019-12-17 04:01:14 +0300

    feat: add matrix 16/9

--- index.js
+++ index.js
@@ -3,12 +3,21 @@ const root = document.createElement('div');
 root.id = 'root';
 const audio = document.createElement('audio');
 // audio.src = './assets/audio/Detsl.mp3';
-audio.src = './assets/audio/Metallica.mp3';
+audio.src = './assets/audio/iranskie.mp3';
+// audio.src = './assets/audio/Metallica.mp3';
 body.appendChild(audio);
 body.appendChild(root);
 
-
-for (let i = 0; i < 50; i++) {
+const shadeOfRed = ['#EB0909', '#FA1E8F', '#E00476', '#FA1E8F', '#F61EFA', '#AD0002', '#AD009C', '#FF0000'];
+const shadeOfYellow = ['#DCD809', '#BBB90A', '#F7F340', '#B2AF0A', '#F78B26', '#D87F2B', '#D8B22B', '#C4D82B'];
+const shadeOfBlue = ['#2BD882', '#12F684', '#69F6AF', '#69F3F6', '#699AF6', '#246DF3', '#0C4CC4', '#6A71F0'];
+const randomColor = [...shadeOfRed, ...shadeOfYellow, ...shadeOfBlue];
+const arrayRandElement = (arr) => {
+  const rand = Math.floor(Math.random() * arr.length);
+  return arr[rand];
+}
+50
+for (let i = 0; i < 144; i++) {
     let square = document.createElement('div');
     square.classList.add('square');
     root.appendChild(square);
@@ -18,11 +27,11 @@ const allSquare = document.querySelectorAll('.square');
 // console.log(allSquare);
 
 allSquare.forEach((el) => {
-    const r = Math.floor(Math.random() * (255)),
-        g = Math.floor(Math.random() * (255)),
-        b = Math.floor(Math.random() * (255)),
-        color = `#${r.toString(16)}${g.toString(16)}${b.toString(16)}`;
-        el.style.background = color;
+    // const r = Math.floor(Math.random() * (255)),
+    //     g = Math.floor(Math.random() * (255)),
+    //     b = Math.floor(Math.random() * (255)),
+        // color = `#${r.toString(16)}${g.toString(16)}${b.toString(16)}`;
+  el.style.background = arrayRandElement(randomColor);
 });
 
 let button = document.createElement('input');
@@ -45,14 +54,13 @@ function loop(){
   if(!audio.paused){
     window.requestAnimationFrame(loop);
   }
-  arr = new Uint8Array(50);
+  arr = new Uint8Array(144);
   analyser.getByteFrequencyData(arr);
   console.log(arr);
-  for (let i = 0; i < 50; i++){
-    const fg = arr[i] / Math.max.apply(null, arr);
+  for (let i = 0; i < 144; i++){
+    const percent = arr[i] / Math.max.apply(null, arr);
     // console.log(arr[i]);
-    // allSquare[i].style.opacity = `${fg}`
-    allSquare[i].style.filter = `brightness(${fg})`;
+    allSquare[i].style.filter = `brightness(${percent})`;
   }
 }
 
